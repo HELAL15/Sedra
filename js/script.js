4172272fd41add74f11333f2fb540f231d439740
HELAL15 <ah4459577@gmail.com>
2024-02-25 15:29:59 +0200

    auth & blog & single-blog

--- js/script.js
+++ js/script.js
@@ -14,6 +14,51 @@ $(document).ready(function(){
 
     new WOW().init();
 
+
+    $(".show-pass").click(function () {
+      $(this).find('i').toggleClass("bi-eye-slash bi-eye");
+      var input = $($(this).attr("toggle"));
+      if (input.attr("type") == "password") {
+          input.attr("type", "text");
+      } else {
+          input.attr("type", "password");
+      }
+      $(this).toggleClass('active');
+    });
+
+
+    const inputElements = [...document.querySelectorAll('input.code')]
+    
+    inputElements.forEach((ele,index)=>{
+      ele.addEventListener('keydown',(e)=>{
+        // if the keycode is backspace & the current field is empty
+        // focus the input before the current. Then the event happens
+        // which will clear the "before" input box.
+        if(e.keyCode === 8 && e.target.value==='') inputElements[Math.max(0,index-1)].focus()
+      })
+      ele.addEventListener('input',(e)=>{
+          inputElements[index].focus()
+        const [first,...rest] = e.target.value
+        e.target.value = first ?? '' // first will be undefined when backspace was entered, so set the input to ""
+        const lastInputBox = index===inputElements.length-1
+        const didInsertContent = first!==undefined
+        if(didInsertContent && !lastInputBox) {
+          // continue to input the rest of the string
+          inputElements[index+1].focus()
+          inputElements[index+1].value = rest.join('')
+          inputElements[index+1].dispatchEvent(new Event('input'))
+        }
+      })
+    })
+    
+    
+    // mini example on how to pull the data on submit of the form
+    function onSubmit(e){
+      e.preventDefault()
+      const code = inputElements.map(({value})=>value).join('')
+      console.log(code)
+    }
+
     
     /* -------------- upload profile pic ---------------- */
     if($('.profile-pic').length > 0){

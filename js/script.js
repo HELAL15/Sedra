955e3ffe808d6547c14fdc30c5b1b78398f579d3
HELAL15 <ah4459577@gmail.com>
2024-02-27 12:16:00 +0200

    edit home. && new profile

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 
 
 
+
     function fixedNav() {
       let scroll = window.scrollY;
       let isNavTop = scroll < 50;
@@ -206,36 +207,116 @@ $(".fav-btn").click(function() {
 });
 
 
+let container_width = $('.container').width();
+
+let offers_body = $('.offers-body').width();
+
+let calc = (offers_body - container_width) - 29 ;
+
+$('.offers-body').css('margin-inline-start', calc);
+
+console.log(calc);
+
+console.log(  `container ${container_width} offers ${offers_body}`);
+
+
+
+var inputLeft = document.getElementById("input-left");
+var inputRight = document.getElementById("input-right");
+
+var thumbLeft = document.querySelector(".slider > .thumb.left");
+var thumbRight = document.querySelector(".slider > .thumb.right");
+var range = document.querySelector(".slider > .range");
+
+if(inputLeft !== null){
+  function setLeftValue() {
+    var _this = inputLeft,
+      min = parseInt(_this.min),
+      max = parseInt(_this.max);
+  
+    _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
+  
+    var percent = ((_this.value - min) / (max - min)) * 100;
+  
+    thumbLeft.style.left = percent + "%";
+    range.style.left = percent + "%";
+  }
+  setLeftValue();
+  
+  function setRightValue() {
+    var _this = inputRight,
+      min = parseInt(_this.min),
+      max = parseInt(_this.max);
+  
+    _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
+  
+    var percent = ((_this.value - min) / (max - min)) * 100;
+  
+    thumbRight.style.right = (100 - percent) + "%";
+    range.style.right = (100 - percent) + "%";
+  }
+  setRightValue();
+  
+  inputLeft.addEventListener("input", setLeftValue);
+  inputRight.addEventListener("input", setRightValue);
+  
+  inputLeft.addEventListener("mouseover", function() {
+    thumbLeft.classList.add("hover");
+  });
+  inputLeft.addEventListener("mouseout", function() {
+    thumbLeft.classList.remove("hover");
+  });
+  inputLeft.addEventListener("mousedown", function() {
+    thumbLeft.classList.add("active");
+  });
+  inputLeft.addEventListener("mouseup", function() {
+    thumbLeft.classList.remove("active");
+  });
+  
+  inputRight.addEventListener("mouseover", function() {
+    thumbRight.classList.add("hover");
+  });
+  inputRight.addEventListener("mouseout", function() {
+    thumbRight.classList.remove("hover");
+  });
+  inputRight.addEventListener("mousedown", function() {
+    thumbRight.classList.add("active");
+  });
+  inputRight.addEventListener("mouseup", function() {
+    thumbRight.classList.remove("active");
+  });
+}
+
 
 
 
 // increment & decrement
-let plus = document.querySelectorAll(".plus");
-if(plus !== null){
-  document.querySelectorAll(".controls").forEach(control => {
-    let plus = control.querySelector(".plus");
-    let minus = control.querySelector(".minus");
-    let result = control.querySelector(".result");
+// let plus = document.querySelectorAll(".plus");
+// if(plus !== null){
+//   document.querySelectorAll(".controls").forEach(control => {
+//     let plus = control.querySelector(".plus");
+//     let minus = control.querySelector(".minus");
+//     let result = control.querySelector(".result");
   
-    let count = 0;
+//     let count = 0;
   
-    plus.addEventListener("click", () => {
-      count++;
-      updateResult();
-    });
+//     plus.addEventListener("click", () => {
+//       count++;
+//       updateResult();
+//     });
   
-    minus.addEventListener("click", () => {
-      if (count > 0) {
-        count--;
-        updateResult();
-      }
-    });
+//     minus.addEventListener("click", () => {
+//       if (count > 0) {
+//         count--;
+//         updateResult();
+//       }
+//     });
   
-    const updateResult = () => {
-      result.value = count
-    };
-  });
-}
+//     const updateResult = () => {
+//       result.value = count
+//     };
+//   });
+// }
 
 
 
